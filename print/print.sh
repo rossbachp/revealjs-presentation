6ab626df0a2b1d98b24315a31f383f274b5e298d
Peter Rossbach <peterrossbach@me.com>
2014-11-17 19:25:47 +0100

    set COPYRIGHT env var

--- print/print.sh
+++ print/print.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-COPYRIGHT='&lt;peter.rossbach@bee42.com&gt;, @PRossbach'
+if [ "$1z" == $"z" ]; then
+  echo "usage: $0 <pdf output filename> <conference title>"
+fi
+DATE=`date +'%Y'`
+if [ "${COPYRIGHT}z" == "z" ]; then
+  COPYRIGHT="${DATE} &lt;peter.rossbach@bee42.com&gt;, @PRossbach"
+  if [ ! "$2z" == $"z" ]; then
+    COPYRIGHT="${COPYRIGHT}, $2"
+  fi
+fi
 ./phantomjs slidefire.js http://127.0.0.1:8000/?print-pdf $1 "$COPYRIGHT"
